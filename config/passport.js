965b8f17213b62520162fa5e11b60606b7d2a0cf
Karthikeyan TS <keyashqa@gmail.com>
2025-06-10 15:32:51 +0530

    jj

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.BASE_URL}/api/auth/google/callback`,
+  callbackURL: `/api/auth/google/callback`,
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
